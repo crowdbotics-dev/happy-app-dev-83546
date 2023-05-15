662a706928c3605c90a22819a43ed88459ef7f10
Crowdbotics <engineering@crowdbotics.com>
2023-05-15 18:03:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "happy-app-dev-83546",
+  url: "https://happy-app-dev-83546.botics.co",
+  api: "https://happy-app-dev-83546.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
